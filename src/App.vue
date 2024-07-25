9e20f4bf2a8a209bd47f9188931a2e2cb45293b0
吴必炫 <2506823133@qq.com>
2024-07-25 23:26:03 +0800

    feat:add watch memo switch.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {onMounted, ref} from 'vue'
+import {onMounted, ref, watch} from 'vue'
 import {bytesToGB, mbToGB} from './utils'
 import {
   Cpu,
@@ -25,15 +25,34 @@ const memory_used = ref({
 const memory_layout = ref<Partial<{
   clockSpeed: number, size: number, type: string
 }>[]>([])
-
+async function queryMemo(){
+  console.log('queryMemo')
+  const memoInfo = await window.services.getMemInfo()
+  memory_used.value = memoInfo
+}
+const watchMemo = ref(false)
+let watchMemoTimerId:NodeJS.Timeout
+function onWatchMemoChange(){
+  watchMemo.value = !watchMemo.value
+}
+watch(watchMemo,()=>{
+  if (watchMemo.value){
+    watchMemoTimerId = setInterval(()=>{
+      queryMemo()
+    },2000)
+  }else {
+    clearInterval(watchMemoTimerId)
+  }
+  console.log('确实观察到变化了')
+})
 
 async function hello() {
   const {brand, physicalCores, performanceCores} = await window.services.getCpuInfo()
-  const memoInfo = await window.services.getMemInfo()
+
   const {vram, model} = await window.services.getGpuInfo()
   const memoryLayout = await window.services.getMemoryLayout()
   memory_layout.value = memoryLayout
-  memory_used.value = memoInfo
+
   cpu_brand.value = brand
   cpu_cores.value = Number(physicalCores)
   cpu_performanceCores.value = Number(performanceCores)
@@ -44,6 +63,7 @@ async function hello() {
 
 onMounted(() => {
   hello()
+  queryMemo()
 })
 
 const vConsole = new VConsole();
@@ -84,20 +104,27 @@ console.log(vConsole)
         <memory-one theme="outline" size="24" fill="#333"/>
       </template>
       <template v-slot:content>
-        <el-descriptions :column="2">
+        <el-descriptions title="运行信息" :column="2">
+          <template v-slot:extra>
+            <span>实时监听：</span><el-switch v-model="watchMemo" :on-change="onWatchMemoChange"/>
+          </template>
           <el-descriptions-item :span="2"  label="总内存">{{ `${bytesToGB(memory_used.total)} GB` }}</el-descriptions-item>
           <el-descriptions-item label="可用">{{ `${bytesToGB(memory_used.available)} GB` }}</el-descriptions-item>
           <el-descriptions-item label="已用">{{ `${bytesToGB(memory_used.active)} GB` }}</el-descriptions-item>
-          <template  v-for="(item,index) in memory_layout">
-            <el-descriptions-item :span="2"  :label="`内存条${index + 1}`">
-              <el-descriptions >
+        </el-descriptions>
+        <el-descriptions title="硬件信息" >
+          <el-descriptions-item>
+            <template  v-for="(item,index) in memory_layout">
+              <el-descriptions :column="4">
+                <el-descriptions-item :label="`内存条${index+1}`"/>
                 <el-descriptions-item label="频率">{{ item.clockSpeed }}</el-descriptions-item>
                 <el-descriptions-item label="类型">{{ item.type }}</el-descriptions-item>
                 <el-descriptions-item label="容量">{{ `${bytesToGB(item.size || 0)} GB` }}</el-descriptions-item>
               </el-descriptions>
-            </el-descriptions-item>
-          </template>
+            </template>
+          </el-descriptions-item>
         </el-descriptions>
+
       </template>
     </OptionCard>
 </template>
